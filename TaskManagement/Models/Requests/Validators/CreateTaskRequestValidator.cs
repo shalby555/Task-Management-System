447ba77975f703fd89dfb10f77702faba12b8084
Sergye_Naughty <shalby555@gmail.com>
2024-11-18 18:36:26 +0300

    CreateTaskRequestValidator

---
+++ TaskManagement/Models/Requests/Validators/CreateTaskRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace TaskManagement.Models.Requests.Validators
+{
+    public class CreateTaskRequestValidator: AbstractValidator<CreateTaskRequest>
+    {
+        public CreateTaskRequestValidator()
+        {
+            RuleFor(model => model.Name).MaximumLength(20).MinimumLength(2);
+            RuleFor(model => model.Description).MaximumLength(150).MinimumLength(20);
+        }
+    }
+}
