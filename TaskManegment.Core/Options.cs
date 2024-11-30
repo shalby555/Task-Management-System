c462ff1af811e405ffaa46a6d5addb2b8917dc3c
Sergye_Naughty <shalby555@gmail.com>
2024-11-30 16:46:08 +0300

    AddOptions

---
+++ TaskManegment.Core/Options.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskManegment.Core;
+
+public class Options
+{
+    public const string ConnectionString = "Host=localhost;Port=5432;Database=Test;Username=postgres;Password=postgres;";
+}
